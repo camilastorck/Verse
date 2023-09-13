f4500c802f4ec4cfaedd571b36f51dbf24b9a8fa
Camila Storck <camila.storck@brubank.com>
2023-09-13 00:35:09 -0300

    Initial Commit

---
+++ Verse/VerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerseApp.swift
+//  Verse
+//
+//  Created by Camila Storck on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct VerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
